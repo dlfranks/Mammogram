5fa9260541956fb9ba97a4567ceec3085b5aa5e5
Deana Franks <dlfranks@ncsu.edu>
2024-08-26 08:15:37 -0400

    updated

---
+++ KMeans.py
@@ -0,0 +1,16 @@
+from numpy import random, array
+
+#Create fake income/age clusters for N people in k clusters
+def createClusteredData(N, k):
+    random.seed(10)
+    pointsPerCluster = float(N)/k
+    X = []
+    for i in range (k):
+        incomeCentroid = random.uniform(20000.0, 200000.0)
+        ageCentroid = random.uniform(20.0, 70.0)
+        for j in range(int(pointsPerCluster)):
+            X.append([random.normal(incomeCentroid, 10000.0), random.normal(ageCentroid, 2.0)])
+    X = array(X)
+    return X
+
+createClusteredData(100, 5)
